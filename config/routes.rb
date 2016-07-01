8b40041d2e683eb6cbc4436f423f3a1812d30268
willjasen <willjasen@gmail.com>
2016-06-30 22:34:37 -0400

    add provider callbacks

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-  match "/:provider/callback" => "sessions#create"
-  match "/signout" => "sessions#destroy", :as => :signout
+  get "/:provider/callback" => "sessions#create"
+  get "/signout" => "sessions#destroy", :as => :signout
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
