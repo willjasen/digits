245ceff64c5636597822aecce711fcffe7def62c
willjasen <willjasen@gmail.com>
2016-07-03 18:13:35 -0400

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160703191230) do
+ActiveRecord::Schema.define(version: 20160703221230) do
 
   create_table "users", force: :cascade do |t|
     t.string   "provider"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20160703191230) do
     t.string   "access_token"
     t.string   "refresh_token"
     t.string   "token_type"
+    t.string   "login"
   end
 
 end
