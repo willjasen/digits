ee5c073ffd702418829bc62c62652dbe0c3a4dda
willjasen <willjasen@gmail.com>
2016-07-02 21:11:34 -0400

    store access_token and refresh_token

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160626220825) do
+ActiveRecord::Schema.define(version: 20160703010949) do
 
   create_table "users", force: :cascade do |t|
     t.string   "provider"
     t.string   "uid"
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.string   "access_token"
+    t.string   "refresh_token"
   end
 
 end
