ec9c8d0bde2c25d462f714f19812231fdcf64401
willjasen <willjasen@gmail.com>
2016-07-03 15:19:31 -0400

    schema catchup..

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160703010949) do
+ActiveRecord::Schema.define(version: 20160703191230) do
 
   create_table "users", force: :cascade do |t|
     t.string   "provider"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20160703010949) do
     t.datetime "updated_at",    null: false
     t.string   "access_token"
     t.string   "refresh_token"
+    t.string   "token_type"
   end
 
 end
